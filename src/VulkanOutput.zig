8e274ecb12f7c80a9a8a69fab8392605ae75dc9d
Rongcui Dong <rongcuid@outlook.com>
2023-01-18 21:28:40 -0500

    Fix crash

--- src/VulkanOutput.zig
+++ src/VulkanOutput.zig
@@ -8,7 +8,8 @@ const zeroInit = std.mem.zeroInit;
 const assert = std.debug.assert;
 const Allocator = std.mem.Allocator;
 
-context: *const VulkanContext,
+instance: c.VkInstance,
+device: c.VkDevice,
 surface: c.VkSurfaceKHR,
 swapchain: c.VkSwapchainKHR,
 swapchainImages: std.ArrayList(c.VkImage),
@@ -44,9 +45,13 @@ pub fn init(context: *const VulkanContext, window: *c.SDL_Window, allocator: All
         c.vkGetSwapchainImagesKHR(context.device, swapchain, &imageCount, images.items.ptr),
         "Failed to get swapchain images",
     );
+    // const formats = try getFormats(allocator, context.physicalDevice, surface);
     // for (images.items) |img| {
     //     const ci = zeroInit(c.VkImageViewCreateInfo, .{
     //         .sType = c.VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
+    //         .image = img,
+    //         .viewType = c.VK_IMAGE_VIEW_TYPE_2D,
+    //         .format = formats.items[0],
     //     });
     //     var view: c.VkImageView = undefined;
     //     vk.check(
@@ -55,7 +60,8 @@ pub fn init(context: *const VulkanContext, window: *c.SDL_Window, allocator: All
     //     );
     // }
     return @This(){
-        .context = context,
+        .instance = context.instance,
+        .device = context.device,
         .surface = surface,
         .swapchain = swapchain,
         .swapchainImages = images,
@@ -65,11 +71,27 @@ pub fn init(context: *const VulkanContext, window: *c.SDL_Window, allocator: All
 pub fn deinit(self: *@This()) void {
     std.log.debug("VulkanOutput.deinit()", .{});
     std.log.debug("Destroying VkSwapchainKHR [0x{x}]", .{@ptrToInt(self.swapchain)});
-    c.vkDestroySwapchainKHR(self.context.device, self.swapchain, null);
+    c.vkDestroySwapchainKHR(self.device, self.swapchain, null);
     self.swapchain = undefined;
-    c.vkDestroySurfaceKHR(self.context.instance, self.surface, null);
+    c.vkDestroySurfaceKHR(self.instance, self.surface, null);
     self.surface = undefined;
-    self.context = undefined;
+    self.device = undefined;
+    self.instance = undefined;
+}
+
+fn getFormats(allocator: Allocator, phys: c.VkPhysicalDevice, surface: c.VkSurfaceKHR) !std.ArrayList(c.VkSurfaceFormatKHR) {
+    var count: u32 = undefined;
+    vk.check(
+        c.vkGetPhysicalDeviceSurfaceFormatsKHR(phys, surface, &count, null),
+        "Failed to get number of surface formats",
+    );
+    var formats = try std.ArrayList(c.VkSurfaceFormatKHR).initCapacity(allocator, count);
+    formats.appendNTimesAssumeCapacity(undefined, count);
+    vk.check(
+        c.vkGetPhysicalDeviceSurfaceFormatsKHR(phys, surface, &count, formats.items.ptr),
+        "Failed to get number of surface formats",
+    );
+    return formats;
 }
 
 fn createSwapchain(
@@ -84,24 +106,16 @@ fn createSwapchain(
         c.vkGetPhysicalDeviceSurfaceCapabilitiesKHR(context.physicalDevice, surface, &capabilities),
         "Failed to get physical device capabilities",
     );
-    var formatCount: u32 = undefined;
-    vk.check(
-        c.vkGetPhysicalDeviceSurfaceFormatsKHR(context.physicalDevice, surface, &formatCount, null),
-        "Failed to get number of surface formats",
-    );
-    var formats = try std.BoundedArray(c.VkSurfaceFormatKHR, 128).init(formatCount);
-    vk.check(
-        c.vkGetPhysicalDeviceSurfaceFormatsKHR(context.physicalDevice, surface, &formatCount, &formats.buffer),
-        "Failed to get number of surface formats",
-    );
+    const formats = try getFormats(std.heap.c_allocator, context.physicalDevice, surface);
+    defer formats.deinit();
     const swapchainCI = c.VkSwapchainCreateInfoKHR{
         .sType = c.VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
         .pNext = null,
         .flags = 0,
         .surface = surface,
         .minImageCount = 3,
-        .imageFormat = formats.buffer[0].format,
-        .imageColorSpace = formats.buffer[0].colorSpace,
+        .imageFormat = formats.items[0].format,
+        .imageColorSpace = formats.items[0].colorSpace,
         // TODO: clamp this
         .imageExtent = c.VkExtent2D{
             .width = width,
