97886ecf5063d7c038e5ff77dd5cbbe21015ddc8
Rongcui Dong <rongcuid@outlook.com>
2023-01-22 19:19:23 -0500

    Roadmap

--- src/c.zig
+++ src/c.zig
@@ -2,9 +2,7 @@ pub usingnamespace @cImport({
     @cInclude("SDL2/SDL.h");
     @cInclude("SDL2/SDL_vulkan.h");
     @cInclude("vulkan/vulkan.h");
-    // @cDefine("VMA_IMPLEMENTATION", {});
     @cInclude("vk_mem_alloc.h");
-    // @cInclude("volk.h");
     // @cDefine("NK_IMPLEMENTATION", {});
     // @cInclude("nuklear/nuklear.h");
 });
