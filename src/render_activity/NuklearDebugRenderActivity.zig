6afd0c852bac34948d321e447d1574d8b6142373
Rongcui Dong <rongcuid@outlook.com>
2023-01-26 15:13:22 -0500

    Should not use SDL_image

--- src/render_activity/NuklearDebugRenderActivity.zig
+++ src/render_activity/NuklearDebugRenderActivity.zig
@@ -5,14 +5,21 @@ const c = @import("../c.zig");
 const vk = @import("../vk.zig");
 const zeroInit = std.mem.zeroInit;
 
+const Pipeline = @import("nk/NuklearPipeline.zig");
+
+allocator: std.mem.Allocator,
 device: c.VkDevice,
+vma: c.VmaAllocator,
+pipeline: Pipeline,
+atlas_texture: vk.Texture,
 
 pub fn init(
+    allocator: std.mem.Allocator,
     device: c.VkDevice,
+    vma: c.VmaAllocator,
 ) !@This() {
     const img_width = 1024;
     const img_height = 1024;
-    var atlas_view: c.VkImageView = undefined;
     var atlas: c.nk_font_atlas = undefined;
     c.nk_font_atlas_init_default(&atlas);
     c.nk_font_atlas_begin(&atlas);
@@ -21,9 +28,33 @@ pub fn init(
         [*]const u8,
         c.nk_font_atlas_bake(&atlas, &img_width, &img_height, c.NK_FONT_ATLAS_RGBA32),
     );
+    // Create device texture
+    var atlas_texture = try vk.Texture.createDefault(
+        allocator,
+        device,
+        vma,
+        img_width,
+        img_height,
+        c.VK_IMAGE_USAGE_SAMPLED_BIT | c.VK_IMAGE_USAGE_TRANSFER_DST_BIT,
+    );
+    const surface = c.SDL_CreateRGBSurfaceFrom(
+        img,
+        img_width,
+        img_height,
+        1,
+        4 * img_width,
+        0xFF000000,
+        0x00FF0000,
+        0x0000FF00,
+        0x000000FF,
+    );
+    var atlas_view: c.VkImageView = undefined;
+    // Finish atlas
     c.nk_font_atlas_end(&atlas, c.nk_handle_ptr(atlas_view), 0);
     return @This(){
+        .allocator = allocator,
         .device = device,
+        .vma = vma,
     };
 }
 
--- src/vk/Texture.zig
+++ src/vk/Texture.zig
@@ -10,6 +10,29 @@ device: c.VkDevice,
 vma: c.VmaAllocator,
 image: c.VkImage,
 alloc: c.VmaAllocation,
+pub fn createDefault(
+    allocator: std.mem.Allocator,
+    device: c.VkDevice,
+    vma: c.VmaAllocator,
+    width: u32,
+    height: u32,
+    usage: c.VkImageUsageFlags,
+) !*@This() {
+    const ci = zeroInit(c.VkImageCreateInfo, .{
+        .sType = c.VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
+        .imageType = c.VK_IMAGE_TYPE_2D,
+        .format = c.VK_FORMAT_R8G8B8A8_UINT,
+        .extent = c.VkExtent3D{ .width = width, .height = height, .depth = 1 },
+        .mipLevels = 1,
+        .arrayLayers = 1,
+        .samples = c.VK_SAMPLE_COUNT_1_BIT,
+        .tiling = c.VK_IMAGE_TILING_OPTIMAL,
+        .usage = usage | c.VK_IMAGE_USAGE_TRANSFER_DST_BIT,
+        .sharingMode = c.VK_SHARING_MODE_EXCLUSIVE,
+        .initialLayout = c.VK_IMAGE_LAYOUT_UNDEFINED,
+    });
+    return create(allocator, device, vma, &ci);
+}
 pub fn create(
     allocator: std.mem.Allocator,
     device: c.VkDevice,
