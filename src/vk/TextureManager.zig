ddd04f2d4e9880b4bc0854bb73b3ec86fc0a7971
Rongcui Dong <rongcuid@outlook.com>
2023-02-04 17:30:51 -0500

    Move loading to manager

--- src/vk/TextureManager.zig
+++ src/vk/TextureManager.zig
@@ -5,6 +5,7 @@ const vk = @import("../vk.zig");
 const zeroInit = std.mem.zeroInit;
 
 const TextureMap = std.StringHashMap(*vk.Texture);
+const Texture = vk.Texture;
 
 allocator: std.mem.Allocator,
 device: c.VkDevice,
@@ -94,6 +95,21 @@ pub fn loadFileRgbaUint(
         @intCast(u32, surface_rgba.*.h),
         usage,
     );
+    try self.load(texture, surface_rgba, layout);
+    try self.textures.put(path, texture);
+    return texture;
+}
+
+/// Load the texture into device right now
+pub fn load(
+    self: *@This(),
+    texture: *Texture,
+    /// Source data
+    surface: *c.SDL_Surface,
+    /// Destination image layout
+    dst_layout: c.VkImageLayout,
+) !void {
+    // Create command buffer
     var cmd: c.VkCommandBuffer = undefined;
     const cmdAI = c.VkCommandBufferAllocateInfo{
         .sType = c.VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
@@ -106,14 +122,160 @@ pub fn loadFileRgbaUint(
         c.vkAllocateCommandBuffers(self.device, &cmdAI, &cmd),
         "Failed to allocate command buffer",
     );
-    try texture.load(
+    defer c.vkFreeCommandBuffers(self.device, self.pool, 1, &cmd);
+    // Create staging buffer
+    var staging: c.VkBuffer = undefined;
+    var stagingAlloc: c.VmaAllocation = undefined;
+    var stagingAI: c.VmaAllocationInfo = undefined;
+    const n_bytes = @intCast(usize, surface.*.w * surface.*.h * surface.*.format.*.BytesPerPixel);
+    const stagingCI = zeroInit(c.VkBufferCreateInfo, .{
+        .sType = c.VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+        .usage = c.VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+        .size = n_bytes,
+        .sharingMode = c.VK_SHARING_MODE_EXCLUSIVE,
+    });
+    const stagingAllocCI = zeroInit(c.VmaAllocationCreateInfo, .{
+        .usage = c.VMA_MEMORY_USAGE_AUTO,
+        .flags = c.VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT | c.VMA_ALLOCATION_CREATE_MAPPED_BIT,
+        .requiredFlags = c.VK_MEMORY_PROPERTY_HOST_COHERENT_BIT | c.VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT,
+    });
+    vk.check(
+        c.vmaCreateBuffer(self.vma, &stagingCI, &stagingAllocCI, &staging, &stagingAlloc, &stagingAI),
+        "Failed to create transfer buffer",
+    );
+    defer c.vmaDestroyBuffer(self.vma, staging, stagingAlloc);
+
+    // Copy image into transfer buffer
+    @memcpy(@ptrCast([*]u8, stagingAI.pMappedData), @ptrCast([*]const u8, surface.*.pixels), n_bytes);
+    // Prepare transfer
+    const beginInfo = c.VkCommandBufferBeginInfo{
+        .sType = c.VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
+        .pNext = null,
+        .flags = c.VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT,
+        .pInheritanceInfo = null,
+    };
+    const region = zeroInit(c.VkBufferImageCopy, .{
+        .imageSubresource = c.VkImageSubresourceLayers{
+            .aspectMask = c.VK_IMAGE_ASPECT_COLOR_BIT,
+            .mipLevel = 0,
+            .baseArrayLayer = 0,
+            .layerCount = 1,
+        },
+        .imageExtent = c.VkExtent3D{
+            .width = @intCast(u32, surface.*.w),
+            .height = @intCast(u32, surface.*.h),
+            .depth = 1,
+        },
+    });
+    // Begin recording
+    vk.check(c.vkBeginCommandBuffer(cmd, &beginInfo), "Failed to begin recording");
+    self.recordUploadTransitionIn(texture, cmd);
+    c.vkCmdCopyBufferToImage(
         cmd,
-        self.queue,
+        staging,
+        texture.image,
+        c.VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+        1,
+        &region,
+    );
+    self.recordUploadTransitionOut(texture, cmd, dst_layout);
+    vk.check(c.vkEndCommandBuffer(cmd), "Failed to end recording");
+    // Submit to queue
+    const cmdInfo = zeroInit(c.VkCommandBufferSubmitInfoKHR, .{
+        .sType = c.VK_STRUCTURE_TYPE_COMMAND_BUFFER_SUBMIT_INFO_KHR,
+        .commandBuffer = cmd,
+    });
+    const submit = zeroInit(c.VkSubmitInfo2KHR, .{
+        .sType = c.VK_STRUCTURE_TYPE_SUBMIT_INFO_2_KHR,
+        .commandBufferInfoCount = 1,
+        .pCommandBufferInfos = &cmdInfo,
+    });
+    vk.check(
+        vk.PfnD(.vkQueueSubmit2KHR).on(self.device)(self.queue, 1, &submit, null),
+        "Failed to submit queue",
+    );
+    vk.check(c.vkQueueWaitIdle(self.queue), "Failed to wait queue idle");
+}
+
+fn recordUploadTransitionIn(
+    self: *const @This(),
+    texture: *Texture,
+    cmd: c.VkCommandBuffer,
+) void {
+    self.recordLayoutTransition(
+        texture,
+        cmd,
+        c.VK_PIPELINE_STAGE_2_NONE_KHR,
+        0,
+        c.VK_PIPELINE_STAGE_2_TRANSFER_BIT_KHR,
+        c.VK_ACCESS_2_TRANSFER_WRITE_BIT_KHR,
+        c.VK_IMAGE_LAYOUT_UNDEFINED,
+        c.VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
         self.transfer_qfi,
         self.graphics_qfi,
-        surface_rgba,
-        layout,
     );
-    try self.textures.put(path, texture);
-    return texture;
+}
+
+fn recordUploadTransitionOut(
+    self: *const @This(),
+    texture: *Texture,
+    cmd: c.VkCommandBuffer,
+    dst_layout: c.VkImageLayout,
+) void {
+    self.recordLayoutTransition(
+        texture,
+        cmd,
+        c.VK_PIPELINE_STAGE_2_TRANSFER_BIT_KHR,
+        c.VK_ACCESS_2_TRANSFER_WRITE_BIT_KHR,
+        c.VK_PIPELINE_STAGE_2_NONE,
+        0,
+        c.VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+        dst_layout,
+        self.transfer_qfi,
+        self.graphics_qfi,
+    );
+}
+
+fn recordLayoutTransition(
+    self: *const @This(),
+    texture: *Texture,
+    cmd: c.VkCommandBuffer,
+    srcStageMask: c.VkPipelineStageFlags2KHR,
+    srcAccessMask: c.VkAccessFlags2KHR,
+    dstStageMask: c.VkPipelineStageFlags2KHR,
+    dstAccessMask: c.VkAccessFlags2KHR,
+    oldLayout: c.VkImageLayout,
+    newLayout: c.VkImageLayout,
+    srcQueueFamilyIndex: u32,
+    dstQueueFamilyIndex: u32,
+) void {
+    const barrier = c.VkImageMemoryBarrier2KHR{
+        .sType = c.VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER_2_KHR,
+        .pNext = null,
+        // Top of pipe, no access
+        .srcStageMask = srcStageMask,
+        .srcAccessMask = srcAccessMask,
+        // Transfer stage write
+        .dstStageMask = dstStageMask,
+        .dstAccessMask = dstAccessMask,
+        // Change to transfer layout
+        .oldLayout = oldLayout,
+        .newLayout = newLayout,
+        .srcQueueFamilyIndex = srcQueueFamilyIndex,
+        .dstQueueFamilyIndex = dstQueueFamilyIndex,
+        .image = texture.image,
+        .subresourceRange = c.VkImageSubresourceRange{
+            .aspectMask = c.VK_IMAGE_ASPECT_COLOR_BIT,
+            .baseMipLevel = 0,
+            .levelCount = 1,
+            .baseArrayLayer = 0,
+            .layerCount = 1,
+        },
+    };
+    const depInfo = zeroInit(c.VkDependencyInfoKHR, .{
+        .sType = c.VK_STRUCTURE_TYPE_DEPENDENCY_INFO_KHR,
+        .imageMemoryBarrierCount = 1,
+        .pImageMemoryBarriers = &barrier,
+    });
+    vk.PfnD(.vkCmdPipelineBarrier2KHR).on(self.device)(cmd, &depInfo);
 }
