7147b257a2d27965660ac33a543aa473ef05debc
Rongcui Dong <rongcuid@outlook.com>
2023-02-07 20:30:35 -0500

    Texture view creation

--- src/render_activity/NuklearDebugRenderActivity.zig
+++ src/render_activity/NuklearDebugRenderActivity.zig
@@ -44,7 +44,17 @@ pub fn init(
         c.VK_IMAGE_USAGE_SAMPLED_BIT,
         c.VK_IMAGE_LAYOUT_READ_ONLY_OPTIMAL_KHR,
     );
-    var atlas_view: c.VkImageView = undefined;
+    var atlas_view = try atlas_texture.createView(
+        c.VK_IMAGE_VIEW_TYPE_2D,
+        c.VK_FORMAT_R8G8B8A8_UINT,
+        c.VkImageSubresourceRange{
+            .aspectMask = c.VK_IMAGE_ASPECT_COLOR_BIT,
+            .baseMipLevel = 0,
+            .levelCount = 1,
+            .baseArrayLayer = 0,
+            .layerCount = 1,
+        },
+    );
     // Finish atlas
     c.nk_font_atlas_end(&atlas, c.nk_handle_ptr(atlas_view), 0);
     return @This(){
@@ -58,7 +68,6 @@ pub fn init(
 }
 
 pub fn deinit(self: *@This()) void {
-    // c.vkDestroyImageView(self.device, self.atlas_view, null);
     self.atlas_texture.destroy();
     // self.pipeline.deinit();
 }
--- src/vk/Texture.zig
+++ src/vk/Texture.zig
@@ -10,6 +10,9 @@ device: c.VkDevice,
 vma: c.VmaAllocator,
 image: c.VkImage,
 alloc: c.VmaAllocation,
+
+views: std.ArrayList(c.VkImageView),
+
 pub fn createDefault(
     allocator: std.mem.Allocator,
     device: c.VkDevice,
@@ -50,16 +53,53 @@ pub fn create(
         "Failed to create image",
     );
     var p = try allocator.create(@This());
+    const views = std.ArrayList(c.VkImageView).init(allocator);
     p.* = @This(){
         .allocator = allocator,
         .device = device,
         .vma = vma,
         .image = image,
         .alloc = alloc,
+        .views = views,
     };
     return p;
 }
 pub fn destroy(self: *@This()) void {
+    for (self.views.items) |v| {
+        c.vkDestroyImageView(self.device, v, null);
+    }
+    self.views.deinit();
     c.vmaDestroyImage(self.vma, self.image, self.alloc);
     self.allocator.destroy(self);
+}
+
+/// Create a managed image view
+pub fn createView(
+    self: *@This(),
+    viewType: c.VkImageViewType,
+    format: c.VkFormat,
+    subresourceRange: c.VkImageSubresourceRange,
+) !c.VkImageView {
+    var view: c.VkImageView = undefined;
+    const ci = c.VkImageViewCreateInfo{
+        .sType = c.VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
+        .pNext = null,
+        .flags = 0,
+        .image = self.image,
+        .viewType = viewType,
+        .format = format,
+        .components = .{
+            .r = c.VK_COMPONENT_SWIZZLE_IDENTITY,
+            .g = c.VK_COMPONENT_SWIZZLE_IDENTITY,
+            .b = c.VK_COMPONENT_SWIZZLE_IDENTITY,
+            .a = c.VK_COMPONENT_SWIZZLE_IDENTITY,
+        },
+        .subresourceRange = subresourceRange,
+    };
+    vk.check(
+        c.vkCreateImageView(self.device, &ci, null, &view),
+        "Failed to create image view",
+    );
+    try self.views.append(view);
+    return view;
 }
